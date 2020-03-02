cc558b99621486fbd324b1d9e479087606e5a988
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-03-02 23:52:36 +0300

    Added fundamentals javascript section.

---
+++ javscript_section/fundamentals.js
@@ -0,0 +1,81 @@
+/*
+Javascript Types
+
+1. Number
+2. String
+3. Boolean
+4. Undefined
+5. Null 
+6. Symbol (ES6)
+7. Object  
+*/
+
+/* 
+Javascript Comparisons
+
+===
+!==
+>
+<
+<=
+>=
+
+*/
+
+// Guess what answers you would get if you ran this in the
+// Javascript Console in Google Chrome. Once you have an answer  to the questions then 
+// check them by copying them and running it in the console yourself line by line 
+
+
+//Evaluate the below:
+5 + "34"
+Soln => 534;
+5 - "4"
+Soln => 1;
+10 % 5
+Soln => 0;
+5 % 10
+Soln => 5;
+"Java" + "Script"
+Soln => "Javascript";
+" " + " "
+Soln => " ";
+" " + 0
+Soln => " 0";
+true + true
+Soln => 2;
+true + false
+Soln => 1;
+false + true
+Soln => 1;
+false - true
+Soln => -1;
+3 - 4
+Soln => -1;
+"Bob" - "bill"
+Soln => NaN;
+
+//Evaluate the below comparisons:
+5 >= 1
+Soln => true;
+0 === 1
+Soln => false;
+4 <= 1
+Soln => false;
+1 != 1
+Soln => false;
+"A" > "B"
+Soln => false
+"B" < "C"
+Soln => true;
+"a" > "A"
+Soln => true;
+"b" < "A"
+Soln => false;
+true === false
+Soln => false;
+true != true
+Soln => false;
+
+// Make the string: "Hi There! It's "sunny" out" by using the + sign:
+Soln => "Hi There! It\'s " + "\"sunny\"" + " out"
