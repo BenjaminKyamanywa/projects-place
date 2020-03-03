6d16b48a56c65abfd7657047e5a063f2e11b1f27
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-03-03 10:37:35 +0300

    Added Datastructures for javascript.

--- javscript_section/fundamentals.js
+++ javscript_section/fundamentals.js
@@ -167,3 +167,79 @@ if (Number(age) < 18) {
     alert("Powering On. Enjoy the ride!");
 }
 
+//1. Make the above code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
+// Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+
+// Soln using function declaration
+function checkDriverAge(){
+    var age = prompt("how old are you?");
+    if (Number(age) < 18) {
+        alert("Sorry, you are too young to drive this car. Powering off");
+    } else if (Number(age) <= 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    } else {
+        alert("Powering On. Enjoy the ride!");
+    }
+}
+
+//2. Create another function that does the same thing, called checkDriverAge2() using Function Expression.
+
+// Soln using function expression
+var checkDriverAge2 = function (){
+    var age = prompt("how old are you?");
+    if (Number(age) < 18) {
+        alert("Sorry, you are too young to drive this car. Powering off");
+    } else if (Number(age) <= 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    } else {
+        alert("Powering On. Enjoy the ride!");
+    }
+}
+//BONUS: Instead of using the prompt. Now, only use console.log and make the checkDriverAge() function accept an argument of age, so that if you enter:
+// it returns "Powering On. Enjoy the ride!"
+
+// Soln 
+function checkDriverAge(age){
+    if (Number(age) < 18) {
+        return "Sorry, you are too young to drive this car. Powering off";
+    } else if (Number(age) === 18) {
+        return "Congratulations on your first year of driving. Enjoy the ride!";
+    } else {
+        return "Powering On. Enjoy the ride!";
+    }
+}
+
+checkDriverAge(92);
+
+/* 
+Datastructure => A way to organise things.
+> Arrays
+> Objects
+*/
+
+// using this array,
+// var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// 1. Remove the Banana from the array.
+Soln => array.shift();
+
+// 2. Sort the array in order.
+Soln => array.sort();
+
+// 3. Put "Kiwi" at the end of the array.
+Soln => array.push("Kiwi");
+
+// 4. Remove "Apples" from the array.
+Soln => array.shift();
+
+// 5. Sort the array in reverse order. (Not alphabetical, but reverse
+// the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
+
+// you should have at the end:
+// ["Kiwi", "Oranges", "Blueberries"]
+Soln => array.reverse();
+
+// using this array,
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges".
+Soln => array2[1][1];
