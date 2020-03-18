0a57fea634f00f35d45d0bc519a14d72e1236b8e
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-03-18 10:51:50 +0300

    added other while loops

--- javscript_section/fundamentals_002.js
+++ javscript_section/fundamentals_002.js
@@ -9,5 +9,19 @@ var todos = [
 
 // for loop
 for ( var i = 0; i < todos.length; i++) {
-    
+    console.log(" I have too" + " " +  todos[i] + "!") ;
 }
+
+// while loop
+var counterOne = 0;
+while (counterOne < 10) {
+    console.log(counterOne);
+    counterOne++;
+}
+
+// do while loop
+var counterTwo = 10
+do {
+    console.log(counterTwo);
+    counterTwo--;
+}while (counterTwo > 0);
