490fbe8a26573d7a77fc11affeb32dc186f6d571
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-03-18 10:41:16 +0300

    added for loop

---
+++ javscript_section/fundamentals_002.js
@@ -0,0 +1,13 @@
+// loops in javaascript
+var todos = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript",
+    "eat healthy"
+];
+
+// for loop
+for ( var i = 0; i < todos.length; i++) {
+    
+}
