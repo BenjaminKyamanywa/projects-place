bff57c9d879015bfffd911bb649883a70b63f5a9
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-03-30 18:45:17 +0300

    added indepth es6

---
+++ javascript_section/es6indepth.js
@@ -0,0 +1,40 @@
+/* 
+
+spread operator : {}
+rest parameters: function(...)
+destructuring assignment: simplifies extracting data on arrays and objects into distinct variables 
+
+*/
+
+
+/* 
+
+    Using the spread operator 
+
+*/
+let a = [20, 40, 50];
+let b = [10, ...a, 70];
+
+let c = ['Constance', 'Jobsteen', 'Creatance'];
+let d = ['Suzana', 'Allison', 'Bobsteen', ...c];
+
+// Using functions
+const collect = (...e) => {
+    console.log(e)
+}
+
+console.log(b);
+console.log(d);
+collect(1,2,3,4,5,6,7,8,9);
+
+/* 
+
+    Destructuring assignment
+
+*/
+
+let f = [1,2,3,4,5,6];
+let [four, five] = f;
+console.log(four, five);
+
+let animals = ['Simba', 'Mufasa', ]
