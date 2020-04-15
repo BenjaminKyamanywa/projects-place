37b8f1951eda03ae2a61219eef7816584440f537
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-04-15 12:22:10 +0300

    edits to logo component of facerecognition app

--- react_projects/facerecognition/src/components/navigation/Logo.js
+++ react_projects/facerecognition/src/components/navigation/Logo.js
@@ -4,8 +4,8 @@ import Tilt from 'react-tilt';
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className="Tilt" options={{ max : 25 }} style={{ height : 250, width: 250 }}>
-                <div className="Tilt-inner">  </div>
+            <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+                 <div className="Tilt-inner"> 👽 </div>
             </Tilt>
         </div>
     );
