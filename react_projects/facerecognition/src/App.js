209fa80e06c8d936227c76e671c6b6b549ab415e
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-04-16 15:21:52 +0300

    added background particles for facerecognition app

--- react_projects/facerecognition/src/App.js
+++ react_projects/facerecognition/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Rank from './components/rank/Rank';
@@ -6,9 +7,23 @@ import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
 
 import Logo from './components/logo/Logo';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 95,
+      density: {
+        enable: true,
+        value_area: 900
+      }
+    }
+  }
+}
+
 const App = () => {
   return (
     <div className="App">
+      <Particles className="particles"
+        params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
