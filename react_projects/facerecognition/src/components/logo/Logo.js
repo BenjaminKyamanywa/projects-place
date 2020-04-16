ebd409ed1d6b2fa9a31f16d864a607ed16eca572
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-04-16 13:00:48 +0300

    added edits to logo component for facerecognition app

--- react_projects/facerecognition/src/components/logo/Logo.js
+++ react_projects/facerecognition/src/components/logo/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-                 <div className="Tilt-inner"> <span role="img" aria-label="image"> 👽 </span> </div>
+                 <div className="Tilt-inner pa3"> <span role="img" aria-label="image"> <img style={{paddingTop: '25px'}} src="https://img.icons8.com/wired/64/000000/brain.png" alt="logo"/> </span> </div>
             </Tilt>
         </div>
     );
