62fb4e38addaa994823a2aea81fac2f5e830efc5
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-04-18 16:42:12 +0300

    edits to getting values for width and height of inputed image url

--- react_projects/facerecognition/src/App.js
+++ react_projects/facerecognition/src/App.js
@@ -32,7 +32,10 @@ const App = (onInputChange, onButtonSubmit, calculateFaceLocation) => {
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('input-image')
+    const image = document.getElementById('input-image');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
   }
 
   onInputChange = (event) => {
