96dcb05de280a4c7852673c9daf2fa2a80da2027
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-04-22 18:01:54 +0300

    added sigin route for facerecognition app

--- react_projects/facerecognition.api/server.js
+++ react_projects/facerecognition.api/server.js
@@ -1,11 +1,47 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const PORT = 4000;
 
 const app = express();
 
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '123',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Welcome to Facerecognition API')
 })
+
+app.post('/signin', (req, res) => {
+    if(req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json('successfull signin!')
+        } else {
+            res.status(400).json('error logging in!')
+        }
+    
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Face Recognition app is listening on port ${PORT}`)
