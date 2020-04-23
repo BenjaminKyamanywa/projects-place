e887a30959f335c65c5eabceff94deb7f70fbfe2
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-04-23 13:35:44 +0300

    added state for signin component and added siginIn email and password states

--- react_projects/facerecognition/src/components/signin/Signin.js
+++ react_projects/facerecognition/src/components/signin/Signin.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Signin = ({ onRouteChange }) => {
+const Signin = ({ onRouteChange }, onEmailChange) => {
+    // Declaring state variables
+    const [signInEmail, setSignInEmail] = useState('');
+    const [signInPassword, setSignInPassword] = useState('');
+
+    onEmailChange = (event) => {
+      setSignInEmail(event.target.value)
+    }
+
+    onPasswordChange = (event) => {
+      setSignInPassword(event.target.value)
+    }
+
     return (
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
