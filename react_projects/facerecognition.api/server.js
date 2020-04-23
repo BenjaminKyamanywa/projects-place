41d4f6c4fc1c556e0e217f7e4ce8905090cf0b3e
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-04-23 11:44:11 +0300

    added cors to facerecognition server

--- react_projects/facerecognition.api/server.js
+++ react_projects/facerecognition.api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const PORT = 4000;
 
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 
 const database = {
