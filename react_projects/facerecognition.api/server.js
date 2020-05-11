9d8a93d131dc9ddf6d7789d68f385218d64b6ea1
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-05-11 16:14:12 +0300

    edits to register route server facerecognition api

--- react_projects/facerecognition.api/server.js
+++ react_projects/facerecognition.api/server.js
@@ -32,37 +32,7 @@ app.get('/', (req, res) => {
 
 app.post('/signin', signin.handleSignin(db, bcrypt))
 
-app.post('/register', (req, res) => {
-    const {email, name, password } = req.body;
-    if(!email || !name || !password){
-        return res.status(400).json('incorrect form submission')
-    }
-    const hash = bcrypt.hashSync(password);
-        db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                    .returning('*')
-                    .insert({
-                        name: name,
-                        email: loginEmail[0],
-                        joined: new Date()
-                    })
-                    .then(user => {
-                        res.json(user[0])
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-        })
-    .catch(err => res.status(400).json('sorry unable to register'))
-    
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
