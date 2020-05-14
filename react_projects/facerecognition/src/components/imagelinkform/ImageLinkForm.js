3efa888a453558d479699f250b5a36bbfd743d7e
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-05-14 19:00:46 +0300

    edits and changes to facerecognition app

--- react_projects/facerecognition/src/components/imagelinkform/ImageLinkForm.js
+++ react_projects/facerecognition/src/components/imagelinkform/ImageLinkForm.js
@@ -4,8 +4,8 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
-            <p>
-            {'This brain will detect faces in your pictures. Give it a shot and see the magic...'}
+            <p className="white">
+            {'This face detection app will detect faces in your pictures. Give it a shot and see the magic...'}
             </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
